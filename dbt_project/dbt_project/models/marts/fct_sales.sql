4272d58e8252c7d72a1359987beee40dff5f0d4c
cnusrini <cnu.srini@hotmail.com>
2026-03-22 11:36:39 -0500

    Module 6 complete - incremental materialization

--- dbt_project/dbt_project/models/marts/fct_sales.sql
+++ dbt_project/dbt_project/models/marts/fct_sales.sql
@@ -1,6 +1,12 @@
+{{ config(materialized='incremental') }}
+
 select
     order_id,
     amount,
-    tax,
-    amount + tax as total_amount
+    amount * 0.1 as tax,
+    amount + (amount * 0.1) as total_amount
 from {{ ref('int_order_metrics') }}
+
+{% if is_incremental() %}
+where order_id > (select max(order_id) from {{ this }})
+{% endif %}
--- dbt_project/dbt_project/models/staging/stg_orders.sql
+++ dbt_project/dbt_project/models/staging/stg_orders.sql
@@ -1,3 +1,3 @@
-select
-    1 as order_id,
-    100 as amount
+select 1 as order_id, 100 as amount
+union all
+select 2 as order_id, 200 as amount
